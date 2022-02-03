8e107f252716526f939bd29e426f6da55e6916b4
dbraunlich <david.johannes.b@gmail.com>
2022-02-03 12:36:57 +0100

    added sound

--- Assets/MyGame/Scripts/door.cs
+++ Assets/MyGame/Scripts/door.cs
@@ -5,12 +5,19 @@ using UnityEngine;
 public class door : MonoBehaviour
 {
     public gamemanager Gamemanager;
+    private AudioSource crush;
+    public Renderer rend;
+    Collider m_Collider;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        crush = GetComponent<AudioSource>();
+        rend = GetComponent<Renderer>();
+        m_Collider = GetComponent<Collider>();
+        m_Collider.enabled = true;
+        rend.enabled = true;
     }
 
     // Update is called once per frame
@@ -27,7 +34,9 @@ public class door : MonoBehaviour
     {
         if (other.gameObject.tag == "pickaxe")
         {
-            Destroy(gameObject);
+            crush.Play(0);
+            rend.enabled = false;
+            m_Collider.enabled = false;
 
         }
     }
